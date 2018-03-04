1bc25fd64c65729b0be6bb16c9653be8cf18f91e
Stefan Martin <ste00martin@gmail.com>
2018-03-03 22:11:26 -0800

    better react native rules

--- environment/react-native.js
+++ environment/react-native.js
@@ -5,12 +5,10 @@ module.exports = {
 	env: {
 		'react-native/react-native': true,
 	},
-	extends: './react.js',
+	extends: ['./react.js', 'plugin:react-native/all'],
 	plugins: ['react-native'],
+
 	rules: {
-		'react-native/no-color-literals': 'error',
-		'react-native/no-inline-styles': 'error',
-		'react-native/no-unused-styles': 'error',
-		'react-native/split-platform-components': 'error',
+		'react-native/no-inline-styles': 'off',
 	},
 }
