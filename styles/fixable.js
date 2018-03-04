3094eb17eadf54719e2eae9a5335f66cf9641b41
Stefan Martin <ste00martin@gmail.com>
2018-03-03 21:31:27 -0800

    adding in sort keys

--- styles/fixable.js
+++ styles/fixable.js
@@ -3,39 +3,66 @@
 // All of these rules will be warnings so its a light nudge in the right direction
 module.exports = {
 	rules: {
+		'max-len': [
+			'warn',
+			{
+				code: 80,
+				ignoreComments: true,
+				ignoreUrls: true,
+				ignoreRegExpLiterals: true,
+			},
+		],
 
-		'comma-dangle': ['warn', {
-			arrays: 'always-multiline',
-			objects: 'always-multiline',
-			imports: 'always-multiline',
-			exports: 'always-multiline',
-			functions: 'always-multiline',
-		}],
+		'comma-dangle': [
+			'warn',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'always-multiline',
+			},
+		],
 
 		// Arguably the most hotly debated rule. TODO: get this to truly fix spacing to tabs in projects
-		indent: ['warn', 'tab', {
-			SwitchCase: 1,
-			VariableDeclarator: 2,
-		}],
+		indent: [
+			'warn',
+			'tab',
+			{
+				SwitchCase: 1,
+				VariableDeclarator: 2,
+			},
+		],
 
 		'import/no-useless-path-segments': 'warn',
 
-		'keyword-spacing': ['warn', {
-			before: true,
-			after: true,
-		}],
+		'keyword-spacing': [
+			'warn',
+			{
+				before: true,
+				after: true,
+			},
+		],
 
-		'linebreak-style': ['error', 'unix'],
-		'lines-between-class-members': ['warn', 'always', {
-			exceptAfterSingleLine: true,
-		}],
+		'lines-between-class-members': [
+			'warn',
+			'always',
+			{
+				exceptAfterSingleLine: true,
+			},
+		],
 
+		// If a console.log is being used in code for good reason in production, it should be calling an api for warning or error reporting like Sentry or Dashbird
 		'no-console': 'warn',
-		'no-extra-parens': ['warn', 'all', {
-			nestedBinaryExpressions: false,
-			returnAssign: false,
-			ignoreJSX: 'multi-line',
-		}],
+		'no-extra-parens': [
+			'warn',
+			'all',
+			{
+				nestedBinaryExpressions: false,
+				returnAssign: false,
+				ignoreJSX: 'multi-line',
+			},
+		],
 		'no-trailing-spaces': 'warn',
 
 		'one-var-declaration-per-line': 'warn',
@@ -48,6 +75,9 @@ module.exports = {
 
 		// Disallow Semicolons
 		semi: ['warn', 'never'],
+
+		'sort-keys': 'warn',
+
 		'space-in-parens': ['warn', 'never'],
 		'spaced-comment': 'warn',
 	},
--- styles/recommended.js
+++ styles/recommended.js
@@ -1,9 +1,10 @@
 'use strict'
 
 module.exports = {
-
 	extends: './fixable.js',
 	rules: {
 		'consistent-this': ['warn', 'self'],
+		'max-nested-callbacks': ['warn', 3],
+		'linebreak-style': ['error', 'unix'],
 	},
 }
