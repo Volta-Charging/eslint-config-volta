d18a8cf89740043c9c7387f7466508f36e7d60f4
Stefan Martin <ste00martin@gmail.com>
2018-03-03 20:19:15 -0800

    adding more fixable rules

--- styles/fixable.js
+++ styles/fixable.js
@@ -1,7 +1,27 @@
 "use strict"
 
+// All of these rules will be warnings so its a light nudge in the right direction
 module.exports = {
   rules: {
-    indent: ["error", "tab"]
+    // Arguably the most hotly debated rule. TODO: get this to truly fix spacing to tabs in projects
+    indent: ["warn", "tab", {
+          SwitchCase: 1,
+    }],
+    'keyword-spacing': ['warn', {
+      before: true,
+      after: true,
+    }],
+    'linebreak-style': ['error', 'unix'],
+    'lines-between-class-members': ['warn', 'always', {
+      exceptAfterSingleLine: true,
+    }],
+    "no-console": "warn",
+    'no-extra-parens': ['warn', 'all', {
+      nestedBinaryExpressions: false,
+      returnAssign: false,
+      ignoreJSX: 'multi-line',
+    }],
+    semi: ["warn", "never"],
+
   }
 }
