22ddfddec2f97b090b3e49fcee62ec175f9d342b
Stefan Martin <ste00martin@gmail.com>
2018-03-03 23:33:32 -0800

    adding in more recommended rules

--- styles/recommended.js
+++ styles/recommended.js
@@ -1,10 +1,17 @@
 'use strict'
 
 module.exports = {
-	extends: './fixable.js',
+	extends: [
+		'eslint:recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'./fixable.js',
+	],
 	rules: {
 		'consistent-this': ['warn', 'self'],
 		'linebreak-style': ['error', 'unix'],
 		'max-nested-callbacks': ['warn', 3],
+		'no-undef': 'off',
+		'operator-linebreak': ['error', 'before'],
 	},
 }
