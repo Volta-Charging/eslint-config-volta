9545a3f0b21248046fdd5d90590bbefd3f87a815
Caleb Brewer <caleb@cazzer.io>
2018-08-08 14:17:59 -0700

    dropping conflicting rule

--- index.js
+++ index.js
@@ -2,11 +2,4 @@
 module.exports = { // eslint-disable-line no-undef
   extends: ['./styles/recommended.js'],
   plugins: ['import'],
-  rules: {
-    'no-multiple-empty-lines': ['error', {
-      max: 0,
-      maxBOF: 0,
-      maxEOF: 1,
-    }],
-  },
 }
