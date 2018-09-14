8d40fd3c9e86e02cc1b31e6de92aea023756c2d2
Ryan Liszewski <ryan@voltacharging.com>
2018-09-14 10:15:58 -0700

    minor upadte to prop rules

--- environment/react.js
+++ environment/react.js
@@ -33,7 +33,7 @@ module.exports = { // eslint-disable-line no-undef
     // This rule checks all JSX multiline elements and verifies the location of the closing bracket.
     'react/jsx-closing-bracket-location': ['warn', 'line-aligned'],
     'react/jsx-first-prop-new-line': [1, 'multiline-multiprop'],
-    'react/jsx-max-props-per-line': [1, { "maximum": 1, "when": 'multiline' }],
+    'react/jsx-max-props-per-line': [1, { "maximum": 1, "when": 'always' }],
     'react/jsx-no-undef': 'error',
     'react/jsx-uses-vars': 'warn',
     'react/no-array-index-key': 'warn',
