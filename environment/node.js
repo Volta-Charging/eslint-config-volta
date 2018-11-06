da361442e2b27d5a40fcfa6eb59489e635025732
Ryan Liszewski <ryan@voltacharging.com>
2018-11-06 09:37:12 -0800

    adding in typescript lintin for node env

--- environment/node.js
+++ environment/node.js
@@ -3,4 +3,10 @@
 module.exports = { // eslint-disable-line no-undef
   env: { node: true },
   globals: { Promise: true },
+  parser: 'typescript-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  settings: { 'import/resolver': { node: { extensions: [ '.ts' ] } } },
 }
