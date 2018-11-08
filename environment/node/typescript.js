8b28e4671868b03aa44da2b0987aa804896bf227
Ryan Liszewski <ryan@voltacharging.com>
2018-11-08 11:35:38 -0800

    moving typescript to it's own env

--- environment/node/typescript.js
+++ environment/node/typescript.js
@@ -1,8 +1,6 @@
 'use strict'
-// coding style for Node modules.
+// coding style for Node modules using typescript.
 module.exports = { // eslint-disable-line no-undef
-  env: { node: true },
-  globals: { Promise: true },
   parser: 'typescript-eslint-parser',
   parserOptions: {
     ecmaVersion: 6,
