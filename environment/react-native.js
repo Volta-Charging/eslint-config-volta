3ae1f943ef0ec097974fc63dde7582d6d194f983
Stefan Martin <ste00martin@gmail.com>
2020-03-26 10:36:37 -0400

    updating react-native rules

--- environment/react-native.js
+++ environment/react-native.js
@@ -4,5 +4,10 @@ module.exports = { // eslint-disable-line no-undef
   env: { 'react-native/react-native': true },
   extends: [ './react.js', 'plugin:react-native/all' ],
   plugins: [ 'react-native' ],
-  rules: { 'react-native/no-inline-styles': 'off' },
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-raw-text': 'off',
+    'react-native/sort-styles': 'warn',
+    'react-native/no-unused-styles': 'warn',
+  },
 }
